a20ae7a1360159f868f83c419543eea38ea3bc5e
Nikhil <nikhil@Nikhils-MacBook-Air.local>
2024-11-18 12:37:56 +0530

    New changes

--- src/pages/gaps/index.tsx
+++ src/pages/gaps/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, ScrollControls } from '@react-three/drei';
 import { Office } from './Office'
 
 
@@ -8,8 +8,11 @@ export default function GapsPage() {
   return (
     <div style={{ height: '100vh', backgroundColor: '#f0f0f0' }}>
       <Canvas>
-        <OrbitControls />
+      <ambientLight intensity={1}/>
+        <OrbitControls enableZoom={false}/>
+        <ScrollControls pages={3} damping={0.5}>
         <Office/>
+        </ScrollControls>
         {/* <mesh>
           <boxGeometry args={[1, 1, 1]} />
           <meshNormalMaterial />
