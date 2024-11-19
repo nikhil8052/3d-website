078494340413c86a99755f1490bcaa989b81e971
Sagmetic <rahulanku1903@gmail.com>
2024-11-19 17:15:00 +0530

    new-model-add

--- src/pages/wall/index.tsx
+++ src/pages/wall/index.tsx
@@ -1,25 +1,39 @@
 'use client';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, ScrollControls } from '@react-three/drei';
-import { Office } from './Office'
-
+import { OrbitControls, ScrollControls, Scroll, PerspectiveCamera } from '@react-three/drei';
+import { Office } from './Office';
+import React from 'react';
 
 export default function GapsPage() {
   return (
-    <div style={{ height: '100vh', backgroundColor: '#f0f0f0' }}>
+    <div style={{ height: '100vh', overflow: 'hidden' }}>
       <Canvas>
-      <ambientLight intensity={1}/>
-        <OrbitControls enableZoom={true}/>
+        <ambientLight intensity={1} />
+        
+        {/* The OrbitControls will let the user rotate around the model, but no zooming */}
+        <OrbitControls enableZoom={false} enablePan={true} />
+
+        {/* ScrollControls to handle the horizontal scrolling */}
         <ScrollControls pages={3} damping={0.5}>
-        <Office/>
+          
+          {/* Custom Camera for the Horizontal Scroll View */}
+          <PerspectiveCamera
+            makeDefault
+            position={[0, 500, 1500]}  // Adjust the camera position to fit the scene well
+            fov={50}
+            near={1}
+            far={10000}
+            rotation={[0, Math.PI, 0]} // Align camera to face the model directly
+          />
+          
+          {/* Here we enable the model to scroll horizontally */}
+          <Scroll>
+            <Office />
+          </Scroll>
+          
         </ScrollControls>
-        {/* <mesh>
-          <boxGeometry args={[1, 1, 1]} />
-          <meshNormalMaterial />
-        </mesh> */}
+        
       </Canvas>
     </div>
   );
 }
-
-
