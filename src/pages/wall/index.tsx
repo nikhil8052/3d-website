aa2417e58312a0cb75bd128646336a5e1512428c
nikk <nikk@gmail.com>
2024-11-21 12:07:57 +0530

    latest changes

--- src/pages/wall/index.tsx
+++ src/pages/wall/index.tsx
@@ -6,34 +6,22 @@ import React from 'react';
 
 export default function GapsPage() {
   return (
-    <div style={{ height: '100vh', overflow: 'hidden' }}>
+    <div style={{ height: 'calc(100vh + 90vh)', overflow: 'hidden' }}>
       <Canvas>
         <ambientLight intensity={1} />
-        
-        {/* The OrbitControls will let the user rotate around the model, but no zooming */}
         <OrbitControls enableZoom={false} enablePan={true} />
-
-        {/* ScrollControls to handle the horizontal scrolling */}
-        <ScrollControls pages={3} damping={0.5}>
-          
-          {/* Custom Camera for the Horizontal Scroll View */}
-          <PerspectiveCamera
-            makeDefault
-            position={[0, 500, 1500]}  // Adjust the camera position to fit the scene well
-            fov={50}
-            near={1}
-            far={10000}
-            rotation={[0, Math.PI, 0]} // Align camera to face the model directly
-          />
-          
-          {/* Here we enable the model to scroll horizontally */}
-          <Scroll>
-            <Office />
-          </Scroll>
-          
-        </ScrollControls>
-        
+        <PerspectiveCamera
+          makeDefault
+          position={[0, 270, 1500]}
+          fov={50}
+          near={1}
+          far={10000}
+          rotation={[0, Math.PI, 0]}
+        />
+        <Office />
       </Canvas>
+     
     </div>
+
   );
 }
