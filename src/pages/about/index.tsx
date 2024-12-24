e624ba1d12998805a70795455673a326289ef022
sourav <you@example.com>
2024-12-24 19:36:36 +0530

    sec/24/24 last code

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -11,6 +11,51 @@ import TextImageList from "@/pages/components/TextImageList";
 import WeWork from "@/pages/components/WeWork";
 
 export default function Page() {
+  const textImageListItems = [
+    {
+      title: "Innovative Technology",
+      imageUrl: "/img/list-chip.png",
+      description:
+        "Discover cutting-edge solutions that redefine industry standards.",
+      ctaLink: "/services",
+    },
+    {
+      title: "Creative Design",
+      imageUrl: "/img/list-chip.png",
+      description:
+        "Experience the synergy of technology and aesthetics in our designs.",
+      ctaLink: "/portfolio",
+    },
+    {
+      title: "Expert Team",
+      imageUrl: "/img/list-chip.png",
+      description:
+        "Meet the brilliant minds driving innovation and excellence.",
+      ctaLink: "/team",
+    },
+    {
+      title: "Global Impact",
+      imageUrl: "/img/list-chip.png",
+      description:
+        "Our projects make a difference on a global scale, empowering communities.",
+      ctaLink: "/impact",
+    },
+    {
+      title: "Sustainable Solutions",
+      imageUrl: "/img/list-chip.png",
+      description:
+        "Commitment to eco-friendly practices and sustainable innovation.",
+      ctaLink: "/sustainability",
+    },
+    {
+      title: "Client Success",
+      imageUrl: "/img/list-chip.png",
+      description:
+        "See how we’ve helped clients achieve their goals through collaboration.",
+      ctaLink: "/success-stories",
+    },
+  ];
+
   return (
     <>
       <HeroBanner
@@ -31,7 +76,7 @@ export default function Page() {
       <WeWork />
       <AboutSlider />
       <WeWork />
-      <TextImageList />
+      <TextImageList items={textImageListItems} />
       <ImageTextGrid />
       <FaqAccordian />
     </>
--- src/pages/components/TextImageList.tsx
+++ src/pages/components/TextImageList.tsx
@@ -1,327 +1,60 @@
-"use client";  // Add this at the top
+"use client";
 
-import React from 'react';
-// import { Html } from '../../../node_modules/next/document';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
-const TextImageList = () => {
+const TextImageList = ({ items }) => {
   return (
-
-
-//     <Image 
-//     className="img-fluid" 
-//     src="/img/laptop-img.png" 
-//     alt="Laptop Image"  width={1400} height={700}
-//     style={{ width: '100%', height: 'auto' }} 
-//   />
-
-  <section className="text-image-list-design bg-brown">
-  <div className="container-fluid hrv position-relative ">
-      <div className="container">
-          <div className="row py_58 ">
-              <div className="col-lg-6">
-                  <div className="col-inner-h ct ">
-                      <h6 className="ct">Dummy Design</h6>
+    <section className="text-image-list-design bg-brown pb_100">
+          {items.map((item, index) => (
+      <div className="container-fluid hrv position-relative">
+        <div className="container">        
+            <div key={index} className="row py_58">
+              <div className="col-lg-3">
+                <div className="col-inner-h ct">
+                  <h6 className="ct">{item.title}</h6>
+                </div>
+              </div>
+              <div className="col-lg-3">
+                <div className="col-inner-h ct col-innner-img">
+                  <div className="dimmy-img">
+                    <Image
+                      className="img-fluid"
+                      src={item.imageUrl}
+                      alt={item.title}
+                      width={1400}
+                      height={700}
+                      style={{ width: "100%", height: "auto" }}
+                    />
                   </div>
+                </div>
               </div>
               <div className="col-lg-6">
-                  <div className="row-info">
-                      <div className="r-info-1 ct">
-                          <p className="m-0"> Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                              accusantium
-                              doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
-                              veritatis et
-                              quasi architecto beatae vitae dicta sunt explicabo. </p>
-                      </div>
-                      <div className="dummy-text-4 mt_58 ml_18">
-                         <div className='view-more-btn'>
-                            <a href="" className="hdr-btn mt_40 text-body">
-                            <span className="arrow">
-                              <i className="fa-solid fa-chevron-right"></i>
-                              </span>
-                              <span className='view-more-txt'>
-                              <span className="view-more-txt-upr">View More</span>
-                      <span className="view-more-txt-btm">View More</span>
-                      </span>
-                        </a>
-                            </div>
-                      </div>
+                <div className="row-info">
+                  <div className="r-info-1 ct">
+                    <p className="m-0">{item.description}</p>
                   </div>
+                  <div className="dummy-text-4 mt_58 ml_18">
+                    <div className="view-more-btn">
+                      <a href={item.ctaLink} className="hdr-btn mt_40 text-body">
+                        <span className="arrow">
+                          <i className="fa-solid fa-chevron-right"></i>
+                        </span>
+                        <span className="view-more-txt">
+                          <span className="view-more-txt-upr">View More</span>
+                          <span className="view-more-txt-btm">View More</span>
+                        </span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
               </div>
-          </div>
+            </div>         
+        </div>
       </div>
-      <div className="dimmy-img">
-      <Image 
-    className="img-fluid" 
-    src="/img/chip.png" 
-    alt="Laptop Image"  width={1400} height={700}
-    style={{ width: '100%', height: 'auto' }} 
-  />
-      </div>
-  </div>
-  <div className="container-fluid hrv position-relative ">
-      <div className="container">
-          <div className="row py_58 ">
-              <div className="col-lg-6">
-                  <div className="col-inner-h ct ">
-                      <h6 className="ct">Dummy Ipsum
-                          Design</h6>
-                  </div>
-              </div>
-              <div className="col-lg-6">
-                  <div className="row-info">
-                      <div className="r-info-1 ct">
-                          <p className="m-0"> Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                              accusantium
-                              doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
-                              veritatis et
-                              quasi architecto beatae vitae dicta sunt explicabo. </p>
-                      </div>
-                      <div className="dummy-text-4  mt_58 ml_18">
-                         <div className='view-more-btn'>
-                            <a href="" className="hdr-btn mt_40 text-body">
-                            <span className="arrow">
-                              <i className="fa-solid fa-chevron-right"></i>
-                              </span>
-                              <span className='view-more-txt'>
-                              <span className="view-more-txt-upr">View More</span>
-                      <span className="view-more-txt-btm">View More</span>
-                      </span>
-                        </a>
-                            </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-      <div className="dimmy-img">
-      <Image 
-    className="img-fluid" 
-    src="/img/chip.png" 
-    alt="Laptop Image"  width={1400} height={700}
-    style={{ width: '100%', height: 'auto' }} 
-  />
-      </div>
-  </div>
-  <div className="container-fluid hrv position-relative ">
-      <div className="container">
-          <div className="row py_58 ">
-              <div className="col-lg-6">
-                  <div className="col-inner-h ct ">
-                      <h6 className="ct">Dummy Design</h6>
-                  </div>
-              </div>
-              <div className="col-lg-6">
-                  <div className="row-info">
-                      <div className="r-info-1 ct">
-                          <p className="m-0"> Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                              accusantium
-                              doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
-                              veritatis et
-                              quasi architecto beatae vitae dicta sunt explicabo. </p>
-                      </div>
-                      <div className="dummy-text-4 mt_30 ml_18">
-                         <div className='view-more-btn'>
-                            <a href="" className="hdr-btn mt_40 text-body">
-                            <span className="arrow">
-                              <i className="fa-solid fa-chevron-right"></i>
-                              </span>
-                              <span className='view-more-txt'>
-                              <span className="view-more-txt-upr">View More</span>
-                      <span className="view-more-txt-btm">View More</span>
-                      </span>
-                        </a>
-                            </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-      <div className="dimmy-img">
-      <Image 
-    className="img-fluid" 
-    src="/img/chip.png" 
-    alt="Laptop Image"  width={1400} height={700}
-    style={{ width: '100%', height: 'auto' }} 
-  />
-      </div>
-  </div>
-  <div className="container-fluid hrv position-relative ">
-      <div className="container">
-          <div className="row py_58 ">
-              <div className="col-lg-6">
-                  <div className="col-inner-h ct ">
-                      <h6 className="ct">Dummy Design</h6>
-                  </div>
-              </div>
-              <div className="col-lg-6">
-                  <div className="row-info">
-                      <div className="r-info-1 ct">
-                          <p className="m-0"> Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                              accusantium
-                              doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
-                              veritatis et
-                              quasi architecto beatae vitae dicta sunt explicabo. </p>
-                      </div>
-                      <div className="dummy-text-4 ml_18">
-                         <div className='view-more-btn'>
-                            <a href="" className="hdr-btn mt_40 text-body">
-                            <span className="arrow">
-                              <i className="fa-solid fa-chevron-right"></i>
-                              </span>
-                              <span className='view-more-txt'>
-                              <span className="view-more-txt-upr">View More</span>
-                      <span className="view-more-txt-btm">View More</span>
-                      </span>
-                        </a>
-                            </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-      <div className="dimmy-img">
-      <Image 
-    className="img-fluid" 
-    src="/img/chip.png" 
-    alt="Laptop Image"  width={1400} height={700}
-    style={{ width: '100%', height: 'auto' }} 
-  />
-      </div>
-  </div>
-  <div className="container-fluid hrv position-relative ">
-      <div className="container">
-          <div className="row py_58 ">
-              <div className="col-lg-6">
-                  <div className="col-inner-h ct ">
-                      <h6 className="ct">Dummy Design</h6>
-                  </div>
-              </div>
-              <div className="col-lg-6">
-                  <div className="row-info">
-                      <div className="r-info-1 ct">
-                          <p className="m-0"> Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                              accusantium
-                              doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
-                              veritatis et
-                              quasi architecto beatae vitae dicta sunt explicabo. </p>
-                      </div>
-                      <div className="dummy-text-4 ml_18">
-                         <div className='view-more-btn'>
-                            <a href="" className="hdr-btn mt_40 text-body">
-                            <span className="arrow">
-                              <i className="fa-solid fa-chevron-right"></i>
-                              </span>
-                              <span className='view-more-txt'>
-                              <span className="view-more-txt-upr">View More</span>
-                      <span className="view-more-txt-btm">View More</span>
-                      </span>
-                        </a>
-                            </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-      <div className="dimmy-img">
-      <Image 
-    className="img-fluid" 
-    src="/img/chip.png" 
-    alt="Laptop Image"  width={1400} height={700}
-    style={{ width: '100%', height: 'auto' }} 
-  />
-  </div>
-  </div>
-  <div className="container-fluid hrv position-relative ">
-      <div className="container">
-          <div className="row py_58 ">
-              <div className="col-lg-6">
-                  <div className="col-inner-h ct ">
-                      <h6 className="ct">Dummy Design</h6>
-                  </div>
-              </div>
-              <div className="col-lg-6">
-                  <div className="row-info">
-                      <div className="r-info-1 ct">
-                          <p className="m-0"> Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                              accusantium
-                              doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
-                              veritatis et
-                              quasi architecto beatae vitae dicta sunt explicabo. </p>
-                      </div>
-                      <div className="dummy-text-4 ml_18 ">
-                         <div className='view-more-btn'>
-                            <a href="" className="hdr-btn mt_40 text-body">
-                            <span className="arrow">
-                              <i className="fa-solid fa-chevron-right"></i>
-                              </span>
-                              <span className='view-more-txt'>
-                              <span className="view-more-txt-upr">View More</span>
-                      <span className="view-more-txt-btm">View More</span>
-                      </span>
-                        </a>
-                            </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-      <div className="dimmy-img">
-      <Image 
-    className="img-fluid" 
-    src="/img/chip.png" 
-    alt="Laptop Image"  width={1400} height={700}
-    style={{ width: '100%', height: 'auto' }} 
-  />
-      </div>
-  </div>
-  <div className="container-fluid hrv position-relative ">
-      <div className="container">
-          <div className="row py_58 ">
-              <div className="col-lg-6">
-                  <div className="col-inner-h ct ">
-                      <h6 className="ct">Dummy Design</h6>
-                  </div>
-              </div>
-              <div className="col-lg-6">
-                  <div className="row-info">
-                      <div className="r-info-1 ct">
-                          <p className="m-0"> Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                              accusantium
-                              doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
-                              veritatis et
-                              quasi architecto beatae vitae dicta sunt explicabo. </p>
-                      </div>
-                      <div className="dummy-text-4 ml_18">
-                         <div className='view-more-btn'>
-                            <a href="" className="hdr-btn mt_40 text-body">
-                            <span className="arrow">
-                              <i className="fa-solid fa-chevron-right"></i>
-                              </span>
-                              <span className='view-more-txt'>
-                              <span className="view-more-txt-upr">View More</span>
-                      <span className="view-more-txt-btm">View More</span>
-                      </span>
-                        </a>
-                            </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-      <div className="dimmy-img">
-      <Image 
-    className="img-fluid" 
-    src="/img/chip.png" 
-    alt="Laptop Image"  width={1400} height={700}
-    style={{ width: '100%', height: 'auto' }} 
-  />
-      </div>
-  </div>
-</section>
-
-
-    );
+       ))}
+    </section>
+  );
 };
 
 export default TextImageList;
